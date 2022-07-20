d0c53f9775a5982972b4a9003f8f875f643e7f81
hainn619 <101760377+hainn619@users.noreply.github.com>
2022-07-20 13:42:27 -0700

    Add files via upload
    113_Assginment_02

---
+++ set_config.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/evn sh
+
+heroku config:set EMAIL_HOST=smtp.sendgrib.net
+heroku config:set EMAIL_HOST_USER=apikey
+heroku config:set EMAIL_HOST_PASSWORD=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
+heroku config:set DEFAULT_FROM_EMAIL=hainn619@gmail.com
